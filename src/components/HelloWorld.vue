681f801541ef0147d419ac67c00affe6be3ab9da
imurlobster <alexander2720@163.com>
2022-08-05 22:40:03 +0800

    feat(custom):
    BREAKING CHANGE:
    init repo vue3-ts project add vuex

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increment">count ++</button>
+  <p>count is {{count}}</p>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import { key } from '@/store'
+
+defineProps<{ msg: string }>()
+
+const store = useStore(key)
+
+const count = computed(() => store.state.count)
+
+const increment = () => {
+  store.commit('increment')
+}
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import { store, key } from './store'
+
+import './style.css'
+import App from './App.vue'
+
+createApp(App).use(store, key).mount('#app')
