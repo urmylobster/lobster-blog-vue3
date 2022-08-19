89777e8084e30d0a8a234186982aecb9abca37b4
imurlobster <alexander2720@163.com>
2022-08-19 18:05:34 +0800

    chore: add tracked files

---
+++ src/utils/type.ts
@@ -0,0 +1,13 @@
+export interface IResponseType<P = {}> {
+  code: number;
+  msg: string;
+  data: P;
+}
+
+export interface IHttpParams {
+  url: string,
+  method: string,
+  data?: object,
+  params?: object
+}
+
---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- {{postData}} -->
+  <div v-for="(item) in postList" :key="item.postId">
+    <h3>{{item.postTitle}}-{{item.postAuthor}}</h3>
+    <p>{{item.postContent}}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref, reactive, toRefs } from 'vue'
+import { getPostsList } from '@/api/api'
+import { PostData, PostRes } from '@/types/index'
+
+let postList = ref([])
+let postData = reactive({
+  list: []
+})
+onMounted(async () => {
+  const res = await getPostsList();
+  postList.value = res;
+  postData.list = res;
+})
+</script>
