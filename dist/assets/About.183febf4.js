b1029f0ad2e7c39d2de61ff1297f76dcd1efbca4
imurlobster <alexander2720@163.com>
2023-02-10 16:42:14 +0800

    chore: build dist

---
+++ dist/assets/About.183febf4.js
@@ -0,0 +1,1 @@
+import{d as o,h as a,c as s,a as e,t as n,F as u,o as l}from"./index.e2d9c9c3.js";const c=e("p",null,"hello about",-1),r=e("p",null,"\u4F60\u597Dabout",-1),d=o({__name:"About",setup(_){const t=a("\u6D4B\u8BD5\u8DEF\u7531");return(p,m)=>(l(),s(u,null,[c,r,e("p",null,n(t.value),1)],64))}});export{d as default};
