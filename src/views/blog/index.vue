7dfc4d955a1333b66b06a4810dbcf5b247b418ad
imurlobster <alexander2720@163.com>
2023-02-10 16:41:40 +0800

    style: tailwind首页样式调整

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="wd-100">
-    <h2 class="text-sm text-orange-600 font-bold">小龙虾🦞的博客</h2>
-    <h6 class="text-xs font-mono mb-2">You are my lobster</h6>
+    <h2 class="text-sm text-orange-600 font-bold mb-2">小龙虾🦞的博客</h2>
+    <h6 class="text-tiny font-mono mb-2">You are my lobster</h6>
     <div class="item mb-4" v-for="(item) in postData.list" :key="item.postId">
-      <h3 class="title">{{item.postTitle}}<p class="mt-1">{{item.postAuthor}}</p>
+      <h3 class="text-tiny py-1 flex font-bol justify-center">
+        <span class="mr-0.5">{{item.postTitle}}</span>
+        <DocumentTextIcon class="inline-block w-2 h-2 align-middle"/>
       </h3>
+      <p class="text-tiny mt-1 leading-normal">{{item.postAuthor}}</p>
       <p class="content">
         <div v-html="item.postContent"></div>
       </p>
@@ -14,6 +17,7 @@
 
 <script lang="ts" setup>
 import { onMounted, ref, reactive, toRefs } from 'vue'
+import { DocumentTextIcon } from '@heroicons/vue/24/solid'
 import { getPostsList } from '@/api/api'
 import { PostData, PostRes } from '@/types/index'
 
@@ -40,16 +44,6 @@ onMounted(async () => {
   background: #0a02c00f;
   border: 2px solid #afafaf;
   border-radius: 12px;
-}
-
-.title {
-  font-size: 20px;
-  font-weight: bolder;
-  padding: 0.35rem 0;
-}
-
-.title p {
-  line-height: 1.5;
 }
 .content {
   font-size: 18px;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,vue}"
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      'tiny': '.5rem'
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
