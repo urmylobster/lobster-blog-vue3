52d8b81483c0b302c92e00d3476629074e56ba77
imurlobster <alexander2720@163.com>
2023-02-14 11:13:27 +0800

    style: 样式调整

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="wd-100">
-    <h2 class="text-sm text-orange-600 font-bold mb-2">小龙虾🦞的博客</h2>
-    <h6 class="text-tiny font-mono mb-2">You are my lobster <br/>
-      <span clas="bg-orange-300 text-orange-600 w-2 h-2 mt-2" @click="addPost">添加文章
-        <PlusCircleIcon class="w-2 h-2 text-orange-300 inline-block align-middle" />
-      </span>
-    </h6>
+    <div class="">
+      <h2 class="text-sm text-orange-600 font-bold mb-2">小龙虾🦞的博客</h2>
+      <h6 class="text-tiny font-mono">You are my lobster</h6>
+      <div class="flex justify-center my-2">
+        <div class="text-center flex items-center text-tiny  bg-orange-300
+        text-orange-600 w-14 h-2 p-2 rounded"
+        @click="addPost">添加文章<PlusCircleIcon class="w-2 h-2" />
+        </div>
+      </div>
+    </div>
     <div class="" v-for="(item) in postData.list" :key="item.postId">
       <h3 class="text-tiny py-1 text-orange-600 flex font-bold">
         <span class="w-10/12 inline-block mr-0.5 font-semibold overflow-hidden
