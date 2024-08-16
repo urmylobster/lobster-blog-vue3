b3634dfeb3e7e9c637f6041bfd7fd60a03b540e8
urmylobster <alexander2720@163.com>
2024-08-16 14:15:39 +0800

    chore: build distribution

--- dist/assets/About.fd6ab0c0.js
+++
@@ -1,1 +0,0 @@
-import{d as o,i as a,c as s,b as e,t as n,F as u,g as l}from"./index.d1dd0b8d.js";const c=e("p",null,"hello about",-1),r=e("p",null,"\u4F60\u597Dabout",-1),d=o({__name:"About",setup(_){const t=a("\u6D4B\u8BD5\u8DEF\u7531");return(p,i)=>(l(),s(u,null,[c,r,e("p",null,n(t.value),1)],64))}});export{d as default};
